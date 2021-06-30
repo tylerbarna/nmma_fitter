0a5e4b9d828002e1d8b6a498884b0b54750211cf
Brandon Reed <reed0657@d.umn.edu>
2021-06-30 10:21:22 -0500

    make_jobs.py now iterates over a list of models

--- make_jobs.py
+++ make_jobs.py
@@ -12,10 +12,11 @@ from fit_utils import parse_csv
 
 search_directory = "./test_dir"
 
+# -TODO- List of jobs? Dictionary of jobs so they can be different for different models?
 job_name = "job.txt"
 
-# -TODO- hopefully this can be turned into a list and we can run multiple models, including the grb model
-model = "Bu2019lm"
+# List of models to run.
+model_list = ["Bu2019lm"]
 
 # -TODO- Can be replaced with something of the form 'filename.log'
 log_filename = "fit.log"
@@ -55,46 +56,53 @@ for ii in range(len(file_list)):
         logfile.write("Not enough data for candidate %s... continuing\n"%candidate_names[ii])
         logfile.close()
         continue
+    #Submit jobs for each model
+    for model in model_list:
+        # -TODO- May want to eliminate shell=True. Apparently there are security holes associated with that.
+        # Submit job
+        command = subprocess.run("sbatch " + job_name + " " + file_list[ii] + " " + candidate_names[ii] + " " + model, shell=True, capture_output=True)
+        output = command.stdout
+        outerr = command.stderr
+        
+        # conver output to an actual string
+        output = str(output, 'utf-8')
+        outerr = str(outerr, 'utf-8')
 
-    # -TODO- May want to eliminate shell=True. Apparently there are security holes associated with that.
-    # Submit job
-    command = subprocess.run("sbatch " + job_name + " " + file_list[ii] + " " + candidate_names[ii] + " " + model, shell=True, capture_output=True)
-    output = command.stdout
-    outerr = command.stderr
-
-    # conver output to an actual string
-    output = str(output, 'utf-8')
-
-    # Job id is generally the last part of the job submission output
-    job_id = int(output.split(' ')[-1])
-    logfile = open(log_filename, "a")
-    logfile.write("Submitted job for candidate %s. Job id: "%candidate_names[ii] + str(job_id) + "\n")
-    logfile.close()
-
-    job_id_list.append(job_id)
-    live_jobs[candidate_names[ii]] = job_id
+        logfile = open(log_filename, "a")
+        logfile.write(outerr)
+        logfile.close()
+                
+        # Job id is generally the last part of the job submission output
+        job_id = int(output.split(' ')[-1])
+        logfile = open(log_filename, "a")
+        logfile.write("Submitted job for candidate " + candidate_names[ii] + ", model " + model + ". Job id: " + str(job_id) + "\n")
+        logfile.close()
+        
+        job_id_list.append(job_id)
+        live_jobs[job_id] = (candidate_names[ii], model)
 
 # Check on jobs every minute to see if they finished.
 # -TODO- Can change the wait time to be reasonable for release
 while len(live_jobs) > 0:
     time.sleep(60)
 
-    finished_candidates = []
-    for candname, id in live_jobs.items():
+    finished_jobs = []
+    for id, (candname, model) in live_jobs.items():
         # nmma_fit makes a .fin file when done
         if os.path.isfile(candname + "_" + model + ".fin"):
             # Do something now that we know the job is done
             logfile = open(log_filename, "a")
-            logfile.write("Job " + str(id) + " for candidate " + candname + " completed. Produced the following output: \n")
+            logfile.write("Job " + str(id) + " for candidate " + candname + " with model " + model + " completed. Produced the following output: \n")
             
             outfile = open(str(id) + ".out", 'r')
             logfile.write(outfile.read())
             outfile.close()
-
+                
             logfile.close()
-
-            finished_candidates.append(candname)
+            
+            finished_jobs.append(id)
             # Check if there were errors?
+            # -TODO- Push the data back.
         elif os.path.isfile(str(id) + ".err") and os.path.getsize(str(id) + ".err") != 0:
             logfile = open(log_filename, "a")
             logfile.write("Job " + str(id) + " for candidate " + candname + " encountered the following errors: \n")
@@ -102,16 +110,17 @@ while len(live_jobs) > 0:
             logfile.write(errorfile.read())
             errorfile.close()
             logfile.close()
-            finished_candidates.append(candname)
+            finished_jobs.append(id)
 
     # update the live job list
-    for candname in finished_candidates:
-        del live_jobs[candname]
+    for id in finished_jobs:
+        del live_jobs[id]
 
 # cleanup: delete finish files and empty error files
-for ii in range(len(file_list)):
-    if os.path.isfile(candidate_names[ii] + "_" + model + '.fin'):
-        os.remove(candidate_names[ii] + "_" + model + '.fin')
+for model in model_list:
+    for ii in range(len(file_list)):
+        if os.path.isfile(candidate_names[ii] + "_" + model + '.fin'):
+            os.remove(candidate_names[ii] + "_" + model + '.fin')
 # can remove .out and .err files as hopefully that information was directed somewhere else (eg. log file)
 for id in job_id_list:
     if os.path.isfile(str(id) + ".err"):
