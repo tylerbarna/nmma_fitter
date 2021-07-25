63719ecd52a287961a21304cf4a9a13511782f37
tylerbarna <tylerpbarna@gmail.com>
2021-07-25 10:16:08 -0500

    automated rsync and fit_bot functionality as part of make_jobs. fit_bot may still require some debugging but is behaving somewhat as expecteed. Created script to manually catch up on data that has not been fit. Need to add complexity so it checks if certain models haven't been run for a candidate

---
+++ fits_catch_up_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH --job-name=catch_up_fits
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ztfrest@gmail.com
+#SBATCH --time=11:59:59
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8gb
+#SBATCH -p small
+#SBATCH -o ./logs/%j.out
+#SBATCH -e ./logs/%j.err
+
+
+cd /panfs/roc/groups/7/cough052/barna314/nmma_fitter/
+
+source /home/cough052/shared/anaconda3/bin/activate nmma
+
+python /panfs/roc/groups/7/cough052/barna314/nmma_fitter/catch_up.py
---
+++ manual_trigger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=manual_nmma_fitter
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ztfrest@gmail.com
+#SBATCH --time=11:59:59
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8gb
+#SBATCH -p small
+#SBATCH -o ./logs/%j.out
+#SBATCH -e ./logs/%j.err
+
+cd /panfs/roc/groups/7/cough052/barna314/nmma_fitter/
+
+source /home/cough052/shared/anaconda3/bin/activate nmma
+
+srun /panfs/roc/groups/7/cough052/barna314/nmma_fitter/primary_job.txt
