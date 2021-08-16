tylerbarna/nmma_fitter

commit a8b62b4f5ea0a56b7d7952a87e38ccc865c2624f

Updated supernova model. Now it is compatible with recent changes to NMMA and is SN only instead of SN+GRB.

nmma_fit.py
@@ -112,7 +112,7 @@ tmin = 0
 tmax = 7
 dt = 0.1
 # GRB model requires special values so lightcurves can be generated without NMMA running into timeout errors.
-if model == "TrPi2018" or model == "nugent-hyper":
+if model == "TrPi2018":
     tmin = 0.01
     tmax = 7.01
     dt = 0.35
@@ -126,9 +126,6 @@ joint_light_curve = False
 sampler = 'pymultinest'
 seed = 42
 
-if model == "nugent-hyper":
-    joint_light_curve = True
-
 #if not os.path.isdir(outdir):
     #os.makedirs(outdir)
     #os.chmod(outdir, 0o774)
@@ -140,30 +137,24 @@ if not os.path.isdir(plotdir):
 
 # Set the prior file. Depends on model and if trigger time is a parameter.
 if prior == None:
-    if joint_light_curve:
-        if model != 'nugent-hyper':
-            #KN+GRB
-            print("Not yet configured for KN+GRB")
-            quit()
+    if model == 'nugent-hyper':
+        # SN
+        if fit_trigger_time:
+            prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_sn_t0.prior'
         else:
-            #supernova
-            if fit_trigger_time:
-                prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_sn_t0.prior'
-            else:
-                prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_sn.prior'
+            prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_sn.prior'
+    if model == 'TrPi2018':
+        # GRB
+        if fit_trigger_time:
+            prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_grb_t0.prior'
+        else:
+            prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_grb.prior'
     else:
-        if model == 'TrPi2018':
-            # GRB
-            if fit_trigger_time:
-                prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_grb_t0.prior'
-            else:
-                prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_grb.prior'
+        # KN
+        if fit_trigger_time:
+            prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_kn_t0.prior'
         else:
-            # KN
-            if fit_trigger_time:
-                prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_kn_t0.prior'
-            else:
-                prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_kn.prior'
+            prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_kn.prior'
 
 # NMMA lightcurve fitting
 # triggered with a shell command
@@ -174,9 +165,6 @@ command_string = "mpiexec -np " + str(cpus) + " light_curve_analysis"\
     + " --tmax " + str(tmax) + " --dt " + str(dt) + " --error-budget " + str(error_budget)\
     + " --nlive " + str(nlive) + " --Ebv-max " + str(Ebv_max)+\
     " --detection-limit \"{\'r\':21.5, \'g\':21.5, \'i\':21.5}\""
-
-if joint_light_curve:
-    command_string += " --joint-light-curve"
 
 command = subprocess.run(command_string, shell=True, capture_output=True)
 sys.stdout.buffer.write(command.stdout)

ZTF_sn.prior
@@ -1,12 +1,4 @@
 luminosity_distance = Uniform(minimum=0, maximum=7000, name='luminosity_distance',latex_label='$D_L$')
-redshift = Uniform(minimum=0.0, maximum=1.0, name='redshift')
-KNtimeshift = 0
+KNtimeshift = 0.
 inclination_EM = Sine(name='inclination_EM', minimum=0, maximum=0.8, latex_label='$\\iota$')
-log10_E0 = Uniform(name='log10_E0', minimum=0, maximum=70, latex_label='$\\log_{10}E_{0}$')
-thetaCore = Uniform(name='thetaCore', minimum=0.01, maximum=np.pi / 2, latex_label='$\\theta_{c}$')
-thetaWing = Uniform(name='thetaWing', minimum=0.01, maximum=np.pi / 2, latex_label='$\\theta_{w}$')
-log10_n0 = Uniform(name='log10_n0', minimum=-4.9, maximum=10, latex_label='$\\log_{10}n_{0}$')
-p = Uniform(name='p', minimum=2, maximum=8, latex_label='$p$')
-log10_epsilon_e = Uniform(name='log10_epsilon_e', minimum=-4, maximum=0, latex_label='$\\log_{10}\\epsilon_{e}$')
-log10_epsilon_B = Uniform(name='log10_epsilon_B', minimum=-5, maximum=0, latex_label='$\\log_{10}\\epsilon_{B}$')
 supernova_mag_boost = Uniform(name='supernova_mag_boost', minimum=-5., maximum=5., latex_label='$\\rm{SNmag}$')

ZTF_sn_t0.prior
@@ -1,12 +1,4 @@
 luminosity_distance = Uniform(minimum=0, maximum=7000, name='luminosity_distance',latex_label='$D_L$')
-redshift = Uniform(minimum=0.0, maximum=1.0, name='redshift')
-KNtimeshift = Uniform(name='t0', minimum=-1.5, maximum=0, latex_label='$t_0$')
+KNtimeshift = Uniform(name='t0', minimum=-1.5, maximum=0.0, latex_label='$t_0$')
 inclination_EM = Sine(name='inclination_EM', minimum=0, maximum=0.8, latex_label='$\\iota$')
-log10_E0 = Uniform(name='log10_E0', minimum=0, maximum=70, latex_label='$\\log_{10}E_{0}$')
-thetaCore = Uniform(name='thetaCore', minimum=0.01, maximum=np.pi / 2, latex_label='$\\theta_{c}$')
-thetaWing = Uniform(name='thetaWing', minimum=0.01, maximum=np.pi / 2, latex_label='$\\theta_{w}$')
-log10_n0 = Uniform(name='log10_n0', minimum=-4.9, maximum=10, latex_label='$\\log_{10}n_{0}$')
-p = Uniform(name='p', minimum=2, maximum=8, latex_label='$p$')
-log10_epsilon_e = Uniform(name='log10_epsilon_e', minimum=-4, maximum=0, latex_label='$\\log_{10}\\epsilon_{e}$')
-log10_epsilon_B = Uniform(name='log10_epsilon_B', minimum=-5, maximum=0, latex_label='$\\log_{10}\\epsilon_{B}$')
 supernova_mag_boost = Uniform(name='supernova_mag_boost', minimum=-5., maximum=5., latex_label='$\\rm{SNmag}$')

commit 7f560a0a060e9831f314c3f523fda96c906b5d53

Fixed typo introduced in commit a8b62b4f5ea0a56b7d7952a87e38ccc865c2624f

nmma_fit.py
@@ -136,6 +136,7 @@ if not os.path.isdir(plotdir):
     os.chmod(plotdir, 0o774)
 
 # Set the prior file. Depends on model and if trigger time is a parameter.
+# -TODO- will want to standardize names to be computable from model name and/or use an argument for the file name.
 if prior == None:
     if model == 'nugent-hyper':
         # SN
@@ -143,18 +144,21 @@ if prior == None:
             prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_sn_t0.prior'
         else:
             prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_sn.prior'
-    if model == 'TrPi2018':
+    elif model == 'TrPi2018':
         # GRB
         if fit_trigger_time:
             prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_grb_t0.prior'
         else:
             prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_grb.prior'
-    else:
+    elif model == 'Bu2019lm':
         # KN
         if fit_trigger_time:
             prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_kn_t0.prior'
         else:
             prior = '/panfs/roc/groups/7/cough052/barna314/nmma_fitter/ZTF_kn.prior'
+    else:
+        print("nmma_fit.py does not know of the prior file for model ", model)
+        exit(1)
 
 # NMMA lightcurve fitting
 # triggered with a shell command