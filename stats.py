620a765ff39718988ebc14bd437c068fec169d42
Tyler Barna <tylerpbarna@gmail.com>
2022-09-07 12:30:03 -0500

    stats plotting start
    I've written a few basic functions that will make plots for some of the stats of the project (it should probably function, but haven't tested on MSI yet). Currently handles the stats for some of the candidate stuff, but none for the fit stats yet. Comments explain further in stats.py.
    
    With the way stats.py is written, it's starting to look at least something like a python package structure, but I'd need to work on that, probably post paper.

--- stats.py
+++ stats.py
@@ -1,12 +1,15 @@
 ## Essentially intended to be a way to create some nice stats plots for the paper
 ## July 2021-July 2022: 1807 candidates, average of 4.36 candidates per day
 
+## Need to stylize the plots
+
 from secrets import choice
 import subprocess
 import sys
 import os
 import argparse
 import glob
+import time
 
 import numpy as np
 import pandas as pd
@@ -25,17 +28,54 @@ import seaborn as sns
 
 ## argument for folder to run stats on
 parser = argparse.ArgumentParser()
-parser.add_argument("-f","--fileDir", type=str, default=None)
+parser.add_argument("-c","--candDir", type=str, default=None, help="Path to the candidate directory")
+parser.add_argument("-f","--fitDir", type=str, default=None, help="Path to the fits directory")
 parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument('-o', '--outdir', type=str, default=os.path.join('./outdir/stats/',time.strftime("%Y%m%d-%H%M%S"),''))
 args = parser.parse_args()
 
-dayList = glob.glob(args.fileDir + "*")
+# if not os.path.exists(args.outdir):
+#         os.makedirs(args.outdir)
+if args.candDir:
+    dayList = glob.glob(args.candDir + "*")
 
-numCandidates = [len(glob.glob(day + "/*.csv")) for day in dayList]
+    dayCount = [day for day in range(0,len(dayList))]
 
-candList = glob.glob(args.fileDir + "*/*.csv")
+    numDaily = [len(glob.glob(day + "/*.csv")) for day in dayList]
 
-# with open(
-# )
+    candList = glob.glob(args.candDir + "*/*.csv")
 
+    cumDaily = np.cumsum(numDaily,axis=1)
+else:
+    print("No candidate directory specified, cannot run some stats")
+
+def plotDir(name,ext=".png"):
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+    filepath = os.path.join(args.outdir,name,ext)
+    return(filepath)
+
+## plot the number of candidates per day
+def plotDailyCand():
+    plt.plot(dayCount, numDaily)
+   #plt.xlabel("Day")
+    plt.savefig(plotDir("numDailyCand"))
+
+## plot the cumulative number of candidates per day
+def plotCumDailyCand():
+    plt.plot(dayCount,cumDaily)
+   #plt.xlabel("Day")
+    plt.savefig(plotDir("cumDailyCand"))
+
+## plot the number of candidates per day with a rolling average
+def plotDailyCandRolling():
+    plt.plot(dayCount, numDaily)
+    plt.plot(dayCount, pd.Series(numDaily).rolling(7).mean())
+    plt.savefig(plotDir("numDailyCandRolling"))
+
+## need to find way to plot time taken to run fits
+## would be done using the fitDir argument and the .log files located in each fit directory
+
+
+    
 
