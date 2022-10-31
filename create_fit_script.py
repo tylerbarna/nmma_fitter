a36357e0d72d41ae123314beb5a7700fc23fde5b
Tyler Barna <tylerpbarna@gmail.com>
2022-10-31 15:01:34 -0500

    Update create_fit_script.py
    not functional, but the start of it is there

--- create_fit_script.py
+++ create_fit_script.py
@@ -16,11 +16,32 @@ model = args.model
 
 
 config = json.load(open('settings.json'))
-
+job_config = config['models'][model]['job']
+job_settings = config['settings']
 
 
 f = open('job.sh','a')
 
 f.write('#!/bin/bash'+'\n')
-f.write
+f.write('#SBATCH --time='+job_config['time']+'\n')
+f.write("#SBATCH --mail-type=ALL"+"\n")
+f.write('#SBATCH --mail-user=ztfrest@gmail.com'+'\n')
+f.write('#SBATCH --nodes='+job_config['nodes']+'\n')
+f.write('#SBATCH --ntasks='+job_config['ntasks']+'\n')
+f.write('#SBATCH --cpus-per-task='+job_config['cpus-per-task']+'\n')
+f.write('#SBATCH --mem='+job_config['mem']+'\n')
+f.write('#SBATCH -p '+job_config['partition']+'\n') 
+f.write('#SBATCH -o %j.out'+'\n')
+f.write('#SBATCH -e %j.err'+'\n')
 
+f.write('\n')
+
+f.write('source '+job_settings['env']['path']+' '+job_settings['env']['name']+'\n')
+
+f.write('\n')
+
+
+## this is where we run into a slight issue - the python script directory is specific to the user/system, so we would either need to add a line to the settings.json file to specify the path to the python script or some other way
+
+## want to have it write something like
+# python /panfs/roc/groups/7/cough052/barna314/nmma_fitter/nmma_fit.py --datafile "$1" --candname "$2" --model "$3" --dataDir "$4" --nlive 128 --cpus 4
