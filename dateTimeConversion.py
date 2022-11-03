e3967dca807c81dad26e3b7313d98256da288cd8
Tyler Barna <tylerpbarna@gmail.com>
2022-11-03 14:58:29 -0500

    Update dateTimeConversion.py
    made conversion

--- dateTimeConversion.py
+++ dateTimeConversion.py
@@ -12,10 +12,25 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 
- 
+from astropy.timeseries import TimeSeries as ts
 from astropy.time import Time
 
 df = pd.read_csv('msiStats/statsDataframe.csv',index_col=0).fillna(value=np.nan)
 print(df.describe())
+#print(df['day'])
 
-df
+## get day column and split based on the - character
+startDay, stopDay = df['day'].str.split('-', 1).str
+startDay, stopDay = startDay.astype('float'), stopDay.astype('float')
+
+print(startDay)
+
+## convert to astropy time
+startDate, stopDate = Time(startDay, format='jd').iso, Time(stopDay, format='jd').iso
+print(startDate)
+df['startDate'] = startDate
+df['stopDate'] = stopDate
+
+df.to_csv('msiStats/statsDataframePrime.csv')
+
+#startDate, stopDate = ts.from_pandas(startDay, 'hjd'), ts.from_pandas(stopDay, 'hjd')
